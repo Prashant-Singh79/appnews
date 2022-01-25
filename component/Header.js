5f37440f297cb10bbb8e69826eeb20dbdcc87aa5
Prashant <prashant.singh@nickelfox.com>
2022-01-25 18:01:36 +0530

    feat(PROJ-NewsApp-Component): Add new Layout and style page

---
+++ component/Header.js
@@ -0,0 +1,21 @@
+import style from "../styles/Header.module.css";
+import Link from "next/link"
+
+function Header() {
+  return (
+    <div className={style.header}>
+      <div className={style.left}>News App</div>
+
+      <ul className={style.list}>
+        <Link href="/">
+        <a className={style.itemList}>Headlines</a>
+        </Link>
+        <Link href="/about">
+        <a className={style.itemList}>Sources</a>
+        </Link>
+      </ul>
+    </div>
+  );
+}
+
+export default Header;
---
+++ component/Layout.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+import Header from "./Header";
+
+function Layout({title,children}) {
+  return (
+  <div>
+    
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <Header/>
+      {children}
+  </div>
+  );
+}
+
+export default Layout;
